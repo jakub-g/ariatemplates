577af60bd4d0677ee8ac14c0b210e53488afbfde
mdadou <matthieu.dadou@amadeus.com>
2013-06-17 14:02:13 +0200

    first commit

---
+++ src/aria/html/beans/RadioButtonCfg.js
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2012 Amadeus s.a.s. Licensed under the Apache License, Version 2.0 (the "License"); you may not use this
+ * file except in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+Aria.beanDefinitions({
+    $package : "aria.html.beans.RadioButtonCfg",
+    $description : "Configuration for RadioButton widget.",
+    $namespaces : {
+        "base" : "aria.html.beans.ElementCfg",
+        "common" : "aria.widgetLibs.CommonBeans"
+    },
+    $beans : {
+        "Properties" : {
+            $type : "base:Properties",
+            $description : "Properties of a RadioButton widget.",
+            $properties : {
+                "name" : {
+                    $type : "json:String",
+                    $description : "The radio button name. Each radio button with the same name will work as a radio buttons group"
+                },
+                "bind" : {
+                    $type : "base:Properties.$properties.bind",
+                    $properties : {
+                        "checked" : {
+                            $type : "common:BindingRef",
+                            $description : "Bi-directional binding. The text input's checked property is set in the bound object."
+                        }
+                    }
+                }
+            }
+        }
+    }
+});
